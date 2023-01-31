6cb9f3d711cc86284b1ad9fc5895a38a7596ec90
TheSilvaProf <fabio01@protonmail.ch>
2023-01-31 08:59:17 -0500

    Conta criada e funcionando.

---
+++ conta4/conta.rb
@@ -0,0 +1,30 @@
+class Conta
+    attr_reader :numero, :titular
+    attr_accessor :saldo
+    def initialize(numero, titular, saldo)
+        @numero = numero
+        @titular = titular
+        @saldo = saldo
+    end
+    def sacar(valor)
+        if saldo >= valor
+            self.saldo -= valor
+        else
+            puts "Saldo insuficiente."               
+        end
+    end
+    def depositar(valor)
+        self.saldo += valor                
+    end
+    def transferir(c_dest, valor)
+        if saldo >= valor
+            self.saldo -= valor
+            c_dest.depositar(valor)
+        else
+            puts "Saldo insuficiente." 
+        end       
+        
+    end
+    
+    
+end
