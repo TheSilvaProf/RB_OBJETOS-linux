afcb7dba46aa47d53f27bf6b4ff55b76a498f1d1
TheSilvaProf <fabio01@protonmail.ch>
2023-01-31 09:05:26 -0500

    Cliente rodando.

---
+++ conta4/cliente.rb
@@ -0,0 +1,8 @@
+require_relative "conta"
+class Cliente
+    attr_accessor :nome, :sobrenome
+    def initialize(nome, sobrenome)
+        @nome = nome
+        @sobrenome = sobrenome
+    end    
+end
